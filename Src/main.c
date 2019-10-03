024168478804661c690e638e70c748bc0de83a36
patrick.reybaud <patrick.reybaud@devisubox.com>
2019-10-03 13:11:56 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -110,9 +110,9 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_MDMA_Init();
+//  MX_MDMA_Init();
   MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
+//  MX_USB_OTG_FS_PCD_Init();
   MX_LWIP_Init();
   /* USER CODE BEGIN 2 */
 	BSP_Config();
