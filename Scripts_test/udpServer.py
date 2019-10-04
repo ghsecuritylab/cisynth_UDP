fbd27db48b67b4fa69895bb1af42134fdd64311b
random-dude <aezurivideo@gmail.com>
2019-10-04 03:11:01 -0400

    visualisations are working on smaller datasets but too sluggish to use on 1000+ samples

--- Scripts_test/udpServer.py
+++ Scripts_test/udpServer.py
@@ -2,13 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import numpy as np
 
 IP = "192.168.0.14"
 port = 7777
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((IP, port))
+# ~ UDPbuffer=np.array([])
+UDPbuffer=[]
+int16perPacket = 5000
 
 while True:
     data, addr = sock.recvfrom(1024)
-    print ("received message:", data)
+    for i in range(int16perPacket*2): # each uint16 is sent as 2 uint8
+        if i%2 != 0 : UDPbuffer.append(int.from_bytes(data[i:i+2], "big", signed=False))
+    print(UDPbuffer, "\n\n")
+    UDPbuffer=[]
+    # ~ print ("received message:", data)
--- Scripts_test/visualisation.py
+++ Scripts_test/visualisation.py
@@ -6,39 +6,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 IP = "192.168.0.14"
-# ~ port = 7777
-port = 2039
-visualisationBuffer = np.array([]*500)
+# ~ IP = "localhost"
+port = 7777
+xResolution, yResolution = 250, 250 # xResolution must be equal to the number of uint16 sent per packet
+visualisationBuffer = np.array([[0]*xResolution]*yResolution)
+listenToUDP = True
 
 def UDPlistener(bufferSize):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((IP, port))
-    while True:
+    while listenToUDP:
         data, addr = sock.recvfrom(bufferSize)
-        UDPbuffer = np.asarray(data)
-        updateVisualisation()
-        print ("received message:", data)
+        UDPbuffer = np.array([])
+        for i in range(xResolution*2) :
+            if i%2 : np.append(UDPbuffer, int.from_bytes(data[i:i+2], "big", signed=False))
+        updateBuffer(UDPbuffer)
+        # ~ print ("received message:", data)
 
-def updateVisualisation() :
+def updateBuffer(line) :
+    global visualisationBuffer
+    visualisationBuffer = np.roll(visualisationBuffer, -1, axis=0)
+    visualisationBuffer[-1] = line
     
-    pass
-    
-def plotData(data):
-    fig, ax = plt.subplots(ncols=1)
+def plotData(ax, data):
     ax.imshow(data, interpolation='none')
-    ax.add_patch(c)
-    plt.show()
-    
+    plt.draw()
+    plt.pause(0.1)
     
 
 if __name__ == '__main__':
     plt.style.use('grayscale')
-    UDPthread = threading.Thread(target = UDPlistener, args=(1024))
+    fig, ax = plt.subplots(figsize=(20 , 20))
+    ax.axis('off')
+    
+    UDPthread = threading.Thread(target = UDPlistener, args=(1024,))
     UDPthread.start()
+    print("listening to UDP on %s:%i" % (IP, port))
+    
     while True : 
          try : 
-             plotData(np.random.random(size=(20, 20)))
+             updateBuffer(np.random.randint(2**16, size=xResolution))
+             plotData(ax,visualisationBuffer)
              time.sleep(.1)
          except KeyboardInterrupt :
-             UDPthread.stop()
+             listenToUDP = False
+             UDPthread.join()
              raise SystemExit
